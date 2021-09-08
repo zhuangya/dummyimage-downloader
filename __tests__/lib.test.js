0809bc47169092824bb903a80b8796df362372ec
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-09-08 18:24:53 +0800

    esm support

--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const path = require('path');
-
-const {
-  getConfig,
-  normalizeConfig,
-  generateImageURL,
-  download
-} = require('../lib');
+import { getConfig } from '../lib.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 test('getConfig should read config from yml, and assign to default config correctly', () => {
   const expectedConfig = {
